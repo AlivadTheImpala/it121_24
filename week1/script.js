0bba9959fd7a28254037da1abc82b12fe7de3820
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2024-09-25 15:27:19 -0700

    added index.html and script.js

---
+++ week1/script.js
@@ -0,0 +1,10 @@
+let firstName = "Brandon";
+let lastName = "Davila";
+let yearsOfStudy = "three";
+let goal =
+  "establish a new careerpath. I have been trying to find what kind of work I enjoy and want to learn about. I enjoy hardware because is it something physical that I can see and visualize. Coding is a bit of a challenge because it deals with a lot of conceptual thinking that is very new for me";
+
+let output = `Hi my name is ${firstName} ${lastName}, and I have been studying at Seattle Central for ${yearsOfStudy} years. My goal is to ${goal}.`;
+
+console.log(output);
+document.write(output);
