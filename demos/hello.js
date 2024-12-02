4903a973c1182cf5e88bdaa8b415450ce43ac917
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2024-12-01 20:03:22 -0800

    added about.html -> week10

--- demos/hello.js
+++ demos/hello.js
@@ -1,1 +1,44 @@
-console.log("Hello, World!");
+// // console.log("Hello, World!");
+// //start demo1
+// let http = require("node:http");
+// listener = function (request, response) {
+//   // Send the HTTP header
+//   // HTTP Status: 200 : OK
+//   // Content Type: text/html
+//   response.writeHead(200, { "Content-Type": "text/html" });
+
+//   // Send the response body as "Hello World"
+//   response.end('<h2 style="text-align: center;">Hello World</h2>');
+// };
+
+// server = http.createServer(listener);
+// server.listen(3000);
+
+// // Console will print the message
+
+// console.log("Server running at http://127.0.0.1:3000/");
+// //end demo1
+
+// //start demo2
+// const args = process.argv; //process.argv returns an array of the process that initiated the code
+
+// console.log(args);
+
+// const firstName = args[2];
+// const lastName = args[3];
+
+// console.log("Hello,", firstName, lastName);
+// //end demo 2
+
+//start demo3
+//imports the Node.js readline module to the readline variable.
+const readline = require("readline").createInterface({
+  input: process.stdin, //creates an input that will take in information
+  output: process.stdout, //allows for the question method to display the query Whats your name? to the terminal and waits for input.
+});
+
+readline.question(`What's your name?`, (name) => {
+  console.log(`Hi ${name}!`);
+  readline.close();
+});
+//end demo 3
--- week10/index.js
+++ week10/index.js
@@ -1,16 +1,27 @@
 import http from "http";
+import fs from "fs";
 const port = process.env.PORT || 3000;
+
+function serveStaticFile(res, path, contentType, responseCode = 200) {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      return res.end("500 - Internal Error");
+    }
+    res.writeHead(responseCode, { "Content-Type": contentType });
+    res.end(data);
+  });
+}
 
 const server = http.createServer((req, res) => {
   const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
   switch (path) {
     case "":
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Homepage");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("<h1>Home Page</h1>");
       break;
     case "/about":
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("About");
+      serveStaticFile(res, "about.html", "text/html");
       break;
     default:
       res.writeHead(404, { "Content-Type": "text/plain" });
